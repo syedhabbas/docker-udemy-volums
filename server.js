f43dc493bb9ca09b0d0b80f2cd329be5273db383
syedhabbas <syedhaiderabbas@gmail.com>
2026-08-08 17:10:59 -0400

    updated

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const fs = require('fs').promises;
 const exists = require('fs').exists;
 const path = require('path');
+require('dotenv').config();
 
 const express = require('express');
 const bodyParser = require('body-parser');
